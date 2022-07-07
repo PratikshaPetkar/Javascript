9a7e353fe07b61694ea5d92fdaf0d1c0d0c12fa8
pratiksha <pratikshapetkar25@gmail.com>
2022-07-07 14:55:04 +0530

    2nd commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//Code With Harry 
+//lecture 2 
+
+console.time('your code took');  // calculates time take by whole code to execute
+console.log('Hello console');   // it outputs the message to web console
+console.log(4+34);
+console.log(78);
+console.log(true);
+console.log([77,52,16,7,6,9]);
+console.log({harry:'this',marks:55});
+console.table({harry:'this',marks:55}); // it represents data in tabular form
+console.warn('this is warning');        // used to give warning message
+// console.clear()                         //it clears the console
+console.timeEnd('your code took');          // calculates time take by whole code to execute
+console.assert(566<189, 'age >189 is not possible') //writes an error message to the console if the assertion is false
+console.error('this is   an error')
+// console.log(0)  //used to write error manaually.
+
+/*
+this 
+is 
+multiline comment
+*/
