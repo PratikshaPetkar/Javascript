d3245e2ffd4b6fdbdae1ef01469207ecc2f9bb43
pratiksha <pratikshapetkar25@gmail.com>
2022-07-17 11:04:32 +0530

    all added

---
+++ Variable2.js
@@ -0,0 +1,57 @@
+// in javaScript variable is a container in which different types of data types can be stored like number,array,strings.
+
+// beauty of JS is we can change the data type of variables in run time. let say we have created one stirng in one variable we can store the number, array and object this concept in programming is known as "DYNAMICALLY TYPED" language (means we can change data type of variable in run time)
+
+//ANALOGY : let say we have case of earbuds so we cant store remote of AC or fan in this case likewise we have different kinds of conatainers(VARIABLES) in JS in what we can store particuler types of data like number,string or collections of numbers and srtings.
+
+
+// so basically this variable in JS is a chunk of memory for example we have room (here cdr memory)in our house and we have different spaces to keep things that different spaces are known as variables.
+
+// in laptop in task manager under memory tab variable will get stored.
+
+
+
+//example : javaScript
+
+
+
+// STATIC TYPED LANGUAGE 
+
+//in this type we have to declare data type of variable first like if it is integer, floating point, string or character then only we can perform operation on that.
+
+// example :  C langauge 
+
+// here we will use let to declare variable.
+
+let b = 67                // a contains 67
+console.log(b)
+b = "harry"               //here we have changed data type of varaible and stored string and we have not used let keyword here coz its already  declared on line 1 so not needed.
+console.log(b)      //op will be 67 and harry
+//this thing only possible in JS
+
+
+
+// let b = 3;  // this is variable declaration
+
+//(keyword to declare variable-->let -->b(identifier)  =  3(literal);
+
+// = called as assignment operator
+
+// Rules for declaring variable
+
+//let 8pra = 7 //not allowed 
+
+// let var = 9 // not allowed coz var is reserverd in js 
+
+// JS is case sensitive 
+// prat and Prat are different it will treated as diff variables.(not recommended)
+
+
+
+
+
+
+
+
+
+
