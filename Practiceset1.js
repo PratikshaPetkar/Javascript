bd1940c64fd4aaf984a377e1b634c468f9c21b85
pratiksha <pratikshapetkar25@gmail.com>
2022-07-27 19:37:42 +0530

    added practice1

---
+++ Practiceset1.js
@@ -0,0 +1,49 @@
+// CHAPTER 1 
+
+// Q1 - create a variable of the string and try to add a number to it 
+
+// let a = "prat"
+// let b = 6
+// console.log(a + b) //it will concatinate a and b
+
+
+// CHAPTER 1
+// Q2 -  use typeof operator to find the datatype of string in prev question
+
+// console.log(typeof (a+b)) //string
+
+//CHAPTER 1
+//Q3 - create const object in JS can you change it to hold a number later?
+
+const a = {
+    name : "Sid",
+    section: 1,
+    isPrincipal: false
+}
+// a = 58; 
+//  a ={}               //we cant reassign the value
+a['friend'] = 'divyanushu'
+a['name'] = 'priya'
+console.log(a)
+
+
+//CHAPTER - Q4
+//try to add new key to abouve const object in problem 3 
+
+// CHAPTER  Q5
+// WAP to create word meaning dictionary of 5 words
+
+const dict = {
+    appreciation : "recognize for good thing",
+    awesome      : "xyzz",
+    yakka :  "hard work"
+
+}
+console.log(dict);
+
+
+
+
+
+
+
