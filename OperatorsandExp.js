0e84faf3146dcb69f4b3e6fd2ca7ae0fdb04d108
pratiksha <pratikshapetkar25@gmail.com>
2022-07-28 17:31:46 +0530

    added operatorsandexp

---
+++ OperatorsandExp.js
@@ -0,0 +1,54 @@
+// Expression : fragment of code that produces a value;
+//belwo are the examples
+// 77;
+// "pratiksha petkar"
+// false
+
+// abouve all are the examples of expsn in js
+
+// console.log("Operators in Js")
+// let a = 8;
+// let b = 4;
+// console.log("a + b =",a+b)
+// console.log("a - b =", a-b)
+// console.log("a / b =", a/b)
+// console.log("a ** b =", a**b)
+// console.log("a % b =", a%b)
+// console.log("++a  =", ++a) //val=9
+// console.log("a++ =", a++)  //10
+// console.log("--a  =", --a) //9
+// console.log("a--  =", a--) //8
+// console.log("a= ",a) //8
+// console.log("a-- =", a--)//8 
+
+//a++ = first it will print then increment 
+//++a = first it will increment the print
+//a-- = first it will print then increment 
+//--a = first it will increment the print
+
+//Assignment Operator
+
+// a = 8
+
+// let assignment = 1;
+// assignment += 5      //same as a = a+5
+// console.log(assignment)
+
+//Comparision Operator
+
+// let comp1 = 6;
+// let comp2 = "6";
+// console.log("comp1 == comp2 is",comp1  ==comp2)
+// console.log("comp1 == comp2 is", comp1 != comp2)
+// console.log("comp1 === comp2 is",comp1 === comp2)
+// console.log("comp1 !==comp2 is", comp1!== comp2)
+// console.log("comp1 > comp2 is", comp1 > comp2)
+
+// // logical operator
+
+// let x = 5;
+// let y = 6;
+// console.log(x<y && x==5)
+// console.log(x>y || x==5)
+// console.log(!false)
+// console.log(!true)
