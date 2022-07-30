4db6366bd4920d7f8ddddfd7c0bb713981a54695
pratiksha <pratikshapetkar25@gmail.com>
2022-07-30 10:15:04 +0530

    added practiceset2

---
+++ PracticeSet2.js
@@ -0,0 +1,63 @@
+//Chapter 2PS
+//Problem No 1:use logical operaror to find whether age of person lies betn 10 and 20
+
+// let age = prompt("what is your age")
+// age =Number.parseInt(age)
+// if(age>10 && age<20) {
+
+//     console.log ("your age lies between 10 and 20")
+// }
+// else{
+//     console.log("your age doesnt lies between 10 and 20")
+// }
+
+//Problem No 2:
+// demo the use case of switch in JS
+// let age = prompt("what is your age")
+// switch (age){
+// case '12':
+// console.log("your age is 12")
+// break
+// case '13':
+// console.log("your age is 13")
+// break 
+// case '14':
+// console.log("your age is 14")
+// break
+// case '15':
+// console.log("your age is 15")
+// break
+// default:
+// console.log("your age is not special")
+// }
+
+//if we enter 12 below all statments will execute it and if we dont want to then put break
+
+//Problem No 3: pgm to find if number is divisible by 2 and 3
+
+// let num = prompt("what is your age")
+// num =Number.parseInt(num)
+//  if(num%2==0 &&num%3==0){
+//     console.log("your num is divisible div by 2 and 3")
+//  }
+//  else{
+//     console.log("your num is not div by 2 and 3")
+//  }
+
+ //Problem No 4: pgm to find if number is divisible by either 2 or 3
+//  let num = prompt("what is your age")
+//   num =Number.parseInt(num)
+//    if(num%2==0){
+//       console.log("your num is divisible div by 2")
+//    }
+//    if(num%3==0){
+//     console.log("your num is divisible div by 3")
+//    }
+//    else{
+//       console.log("your num is not div by 2 and 3")
+//    }
+
+//Problem No 5: print "you can drive" or "you cannot drive"based on age >18 using ternary
+// let age = 19
+// let a = age>18? "you can drive":"you cannot drive"
+// console.log(a)
